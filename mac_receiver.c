30db1f5de4cbfcdb64d5b313b1b3095e2afea136
Gilles Mottiez <gilles.mottiez@students.hevs.ch>
2018-12-03 16:12:02 +0100

    start of implementation

--- mac_receiver.c
+++ mac_receiver.c
@@ -17,7 +17,68 @@
 //////////////////////////////////////////////////////////////////////////////////
 void MacReceiver(void *argument)
 {
-
+	struct queueMsg_t queueMsg;		// queue message
+	uint8_t * msg;		// pointer on the message (frame without STX/ETX)
+	uint8_t * qPtr;  // pointer on the frame
+	size_t	size;
+	osStatus_t retCode;
+	
+	for(;;)
+	{
+		//----------------------------------------------------------------------------
+		// QUEUE READ	from queue_macR_id									
+		//----------------------------------------------------------------------------
+		retCode = osMessageQueueGet( 	
+			queue_macR_id,
+			&queueMsg,
+			NULL,
+			osWaitForever); 	
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);				
+		qPtr = queueMsg.anyPtr;
+		
+		//---------------------------------------------------------------------------
+		// DETECT TYPE OF FRAME
+		//---------------------------------------------------------------------------
+			
+		// TOKEN RECEIVED
+		if(qPtr[1] == TOKEN_TAG)   
+		{
+			queueMsg.type = TOKEN;
+			//--------------------------------------------------------------------------
+			// QUEUE SEND	(send received frame to mac layer sender)
+			//--------------------------------------------------------------------------
+			retCode = osMessageQueuePut(
+				queue_macS_id,
+				&queueMsg,
+				osPriorityNormal,
+				osWaitForever);
+			CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+		
+		}	
+		// DATA FRAME RECEIVED
+		else												
+		{
+			// it's a data so get the msg
+			memcpy(msg,&qPtr[1],size-2);
+			
+			// We are the destination it's a DATA_IND for one of our SAPI
+			if(((msg[1]>>3) == gTokenInterface.myAddress) ||	// is destination my address (CHAT SAPI)
+				((msg[1]>>3) == BROADCAST_ADDRESS))	// is a broadcast frame (TIME SAPI)
+			{
+				// to be done update type please
+				
+			}
+			
+			// We were the source so this is a DATABACK
+			else if(((msg[0]>>3) == gTokenInterface.myAddress))	// is source my address
+			{
+				// to be done
+				queueMsg.type = DATABACK;
+			}
+		}
+		
+		
+	}
 }
 
 
--- mac_sender.c
+++ mac_sender.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-#include "ext_led.h"
+#include "ext_led.h" 
 
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -18,7 +18,68 @@
 //////////////////////////////////////////////////////////////////////////////////
 void MacSender(void *argument)
 {
+	struct queueMsg_t queueMsg;		// queue message
+	uint8_t * msg;		// pointer on the message (frame without STX/ETX)
+	uint8_t * qPtr;  // pointer on the frame
+	size_t	size;
+	osStatus_t retCode;
+	
+	osMessageQueueId_t local_buff_id;
+	const osMessageQueueAttr_t local_buff_attr = {
+		.name = "LOCAL_BUFF"  	
+	};
 
+	local_buff_id = osMessageQueueNew(4,sizeof(struct queueMsg_t),NULL); 		
+	
+	for(;;)
+	{
+		//----------------------------------------------------------------------------
+		// QUEUE READ	from queue_macS_id									
+		//----------------------------------------------------------------------------
+		retCode = osMessageQueueGet( 	
+			queue_macS_id,
+			&queueMsg,
+			NULL,
+			osWaitForever); 	
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);				
+		qPtr = queueMsg.anyPtr;
+		
+		//DATABACK RECEIVED
+		if(queueMsg.type == DATABACK)
+		{
+		}
+		
+		//TOKEN RECEIVED
+		else if(queueMsg.type == TOKEN)
+		{
+			
+		}
+		
+		//NEW TOKEN REQUEST
+		else if(queueMsg.type == NEW_TOKEN)
+		{
+			//set the time sapi as active
+			gTokenInterface.station_list[gTokenInterface.myAddress] = 0b00001000;
+			
+			//create the token frame
+			
+		}
+		
+		//START REQUEST (connect chat)
+		else if(queueMsg.type == START)
+		{
+		}
+		
+		//STOP REQUEST (disconnect chat)
+		else if(queueMsg.type == STOP)
+		{
+		}
+		
+		//GOT DATA FROM OUR SAPIs
+		else if(queueMsg.type == DATA_IND)
+		{
+		}
+	}
 }
 
 
